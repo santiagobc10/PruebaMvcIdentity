a03c4b0b4dd8d607ad8429507a9cf295fee5f1b7
Edwar Becerra <santiagobecerraalarcon@gmail.com>
2024-12-03 09:33:09 -0500

    Aplicacion Prueba Identity

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PruebaMvcIdentity.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PruebaMvcIdentity.Startup))]
+namespace PruebaMvcIdentity
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
